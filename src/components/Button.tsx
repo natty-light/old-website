d60a2dac7036f4aa7058d238610d1db9da9f1df3
natty-light <75773163+natty-light@users.noreply.github.com>
2023-01-28 10:34:57 -0500

    Feat(Buttons) Add button component and styles - Add Button.tsx - Add Button.module.css - Add types.ts - Add <Button /> to index.astro

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import type { Component } from 'solid-js';
+import type { ButtonProps } from '../types';
+import styles from '../styles/button.module.css'
+
+const Button: Component<ButtonProps> = (props) => {
+
+  const clickHandler = (e:  MouseEvent & { currentTarget: HTMLAnchorElement; target: Element;}) => {
+    e.preventDefault()
+    window.open(props.destination)
+}
+
+  return (
+    <>
+      <a
+        class={styles.button}
+        href='#'
+        onClick={(e) => clickHandler(e)}
+      >
+        <div>
+          {props.content}
+        </div>
+      </a>
+    </>
+  )
+}
+
+export default Button;
