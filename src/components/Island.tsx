f53c304ed6c70acf677f0847c4553ae542e46e50
natty-light <75773163+natty-light@users.noreply.github.com>
2023-02-08 23:45:09 -0500

    Improve Copy Text

--- src/components/Island.tsx
+++ src/components/Island.tsx
@@ -6,9 +6,12 @@ const Island: Component = () => {
   return (
     <div class={styles.main}>
       <h1 class={styles.toptext}>Hi, I'm <span class={styles.fancytext}>Nat</span></h1>
-      <p>I'm a developer with a penchant for silly projects using Go and OpenGL</p>
-      <p>You've found my Work-In-Progress site which I am building using Astro and SolidJS</p>
-      <p>For now, if you like, here are some of my projects, or alternatively, some places you can find out more about me</p>
+      <p class={styles.paragraph}>
+        I'm a developer with a penchant for silly projects using Go and OpenGL. <br/>
+        My background is in physics but I currently work as a Full Stack Developer Intern <br/>
+        You've found my Work-In-Progress site which I am building using Astro and SolidJS <br/><br/>
+        For now, if you like, here are some of my projects, or alternatively, some places you can find out more about me
+      </p>
     </div>
   );
 }
