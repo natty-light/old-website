0d5ac7b231c5617aeb236d2f0b26d4eb5915e166
natty-light <75773163+natty-light@users.noreply.github.com>
2023-02-09 21:40:46 -0500

    Upgrade to Astro 2.0.10

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ngallant.dev',
+  site: 'https://ngallant.dev/',
   integrations: [solidJs()]
 });
